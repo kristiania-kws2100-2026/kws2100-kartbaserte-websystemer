f13b0bfd6c85067b59d1f19ed0659eb93963a153
Johannes Brodwall <jhannes@gmail.com>
2026-01-04 13:25:24 +0100

    display OpenStreetMap focused on Oslo
    * `layers: [new TileLayer({source: new OSM()}]`
    * `view: new View({center: [11, 60], zoom: 7})`

--- src/components/app/application.tsx
+++ src/components/app/application.tsx
@@ -1,7 +1,18 @@
 import React, { useEffect, useRef } from "react";
-import { Map } from "ol";
+import { Map, View } from "ol";
+import { OSM } from "ol/source.js";
+import TileLayer from "ol/layer/Tile.js";
+import { useGeographic } from "ol/proj.js";
 
-const map = new Map();
+useGeographic();
+
+const map = new Map({
+  layers: [new TileLayer({ source: new OSM() })],
+  view: new View({
+    center: [11, 60],
+    zoom: 10,
+  }),
+});
 
 export function Application() {
   const mapRef = useRef<HTMLDivElement | null>(null);
