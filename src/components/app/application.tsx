ab8fde9904b24ee0bcc2083a8068b3538b7b1ea4
Johannes Brodwall <jhannes@gmail.com>
2026-01-05 12:56:43 +0100

    show fylker

--- src/components/app/application.tsx
+++ src/components/app/application.tsx
@@ -2,11 +2,20 @@ import React, { useEffect, useRef } from "react";
 import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile.js";
 import { OSM } from "ol/source.js";
+import VectorLayer from "ol/layer/Vector.js";
+import VectorSource from "ol/source/Vector.js";
+import { GeoJSON } from "ol/format.js";
 
 const map = new Map({
   layers: [
     new TileLayer({
       source: new OSM(),
+    }),
+    new VectorLayer({
+      source: new VectorSource({
+        url: "/geojson/fylker.geojson",
+        format: new GeoJSON(),
+      }),
     }),
   ],
   view: new View({
