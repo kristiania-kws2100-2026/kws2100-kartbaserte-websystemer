e461ce4c8e0e1656a77e66d4bfc2994b48c638ef
Johannes Brodwall <jhannes@gmail.com>
2026-01-11 21:52:03 +0100

    introduce the starting code from the Thinking in React tutorial

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
-import { Application } from "./components/app/application.js";
+import App from "./components/app/application.js";
 
-createRoot(document.getElementById("root")!).render(
-  <Application description={"React Application"} />,
-);
+createRoot(document.getElementById("root")).render(<App />);
