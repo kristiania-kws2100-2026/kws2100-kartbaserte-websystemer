1a190caae76fce422f05efc498d5983e2a213de7
Johannes Brodwall <jhannes@gmail.com>
2026-01-11 21:22:45 +0100

    test code formatting with prettier
    * `npm i -D prettier`
    * `npm pkg set scripts.test="prettier --check ."`

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
 import {createRoot} from "react-dom/client"
 import React from "react";
 
-createRoot(document.getElementById("root")).render(<h1>Hello World</h1>)
+createRoot(document.getElementById("root")).render(
+    <h1>
+        Hello World</h1>)
