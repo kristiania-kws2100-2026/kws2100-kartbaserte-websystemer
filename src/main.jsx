86cca44d20230ed578916f82f70c5dc83f5809ef
Johannes Brodwall <jhannes@gmail.com>
2026-01-12 12:59:47 +0100

    import starting code from Thinking in React

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./app";
+import App from "./app";
 
 createRoot(document.getElementById("app")).render(<App />);
