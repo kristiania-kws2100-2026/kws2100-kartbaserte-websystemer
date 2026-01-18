b7c940002634b9000e94e565c3b374f34ae0602a
Johannes Brodwall <jhannes@gmail.com>
2026-01-18 21:56:39 +0100

    change fylke style on hover (show fylkesnavn)

--- src/components/app/application.tsx
+++ src/components/app/application.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef } from "react";
-import { Map, View } from "ol";
+import React, { useEffect, useRef, useState } from "react";
+import { Feature, Map, MapBrowserEvent, View } from "ol";
 import TileLayer from "ol/layer/Tile.js";
 import { OSM } from "ol/source.js";
 import { useGeographic } from "ol/proj.js";
@@ -9,14 +9,18 @@ import "./application.css";
 import VectorLayer from "ol/layer/Vector.js";
 import VectorSource from "ol/source/Vector.js";
 import { GeoJSON } from "ol/format.js";
+import { round } from "ol/math.js";
+import { Style, Text } from "ol/style.js";
+import type { FeatureLike } from "ol/Feature.js";
 
 useGeographic();
 
+const fylkeSource = new VectorSource({
+  url: "/kws2100-kartbaserte-websystemer/geojson/fylker.geojson",
+  format: new GeoJSON(),
+});
 const fylkeLayer = new VectorLayer({
-  source: new VectorSource({
-    url: "/kws2100-kartbaserte-websystemer/geojson/fylker.geojson",
-    format: new GeoJSON(),
-  }),
+  source: fylkeSource,
 });
 const kommuneLayer = new VectorLayer({
   source: new VectorSource({
@@ -29,11 +33,32 @@ const map = new Map({
   view: new View({ center: [11, 59], zoom: 8 }),
 });
 
+function activeFylkeStyle(fylke: FeatureLike) {
+  const { geometry, ...properties } = fylke.getProperties();
+  return new Style({
+    text: new Text({
+      text: fylke.getProperties()["fylkesnavn"],
+    }),
+  });
+}
+
 export function Application() {
   const mapRef = useRef<HTMLDivElement | null>(null);
+  const [activeFylke, setActiveFylke] = useState<Feature>();
+
+  function handlePointerMove(event: MapBrowserEvent) {
+    const fylke = fylkeSource.getFeaturesAtCoordinate(event.coordinate);
+    setActiveFylke(fylke.length > 0 ? fylke[0] : undefined);
+  }
+
+  useEffect(() => {
+    activeFylke?.setStyle(activeFylkeStyle);
+    return () => activeFylke?.setStyle(undefined);
+  }, [activeFylke]);
 
   useEffect(() => {
     map.setTarget(mapRef.current!);
+    map.on("pointermove", handlePointerMove);
   }, []);
 
   return (
