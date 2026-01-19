ab96ac82356bddedda36ae3983282dfa7b2584d7
Johannes Brodwall <jhannes@gmail.com>
2026-01-19 13:26:21 +0100

    pointer move tracks active fylke

--- src/components/app/application.tsx
+++ src/components/app/application.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef } from "react";
-import { Map, View } from "ol";
+import React, { useEffect, useRef, useState } from "react";
+import { Feature, Map, MapBrowserEvent, View } from "ol";
 import TileLayer from "ol/layer/Tile.js";
 import { OSM } from "ol/source.js";
 import { useGeographic } from "ol/proj.js";
@@ -12,11 +12,12 @@ import { GeoJSON } from "ol/format.js";
 
 useGeographic();
 
+const fylkeSource = new VectorSource({
+  url: "/kws2100-kartbaserte-websystemer/geojson/fylker.geojson",
+  format: new GeoJSON(),
+});
 const fylkeLayer = new VectorLayer({
-  source: new VectorSource({
-    url: "/kws2100-kartbaserte-websystemer/geojson/fylker.geojson",
-    format: new GeoJSON(),
-  }),
+  source: fylkeSource,
 });
 const layers = [new TileLayer({ source: new OSM() }), fylkeLayer];
 
@@ -27,14 +28,27 @@ const map = new Map({
 
 export function Application() {
   const mapRef = useRef<HTMLDivElement | null>(null);
+  const [activeFylke, setActiveFylke] = useState<Feature>();
+
+  function handlePointermove(e: MapBrowserEvent) {
+    let fylkeUnderPointer = fylkeSource.getFeaturesAtCoordinate(e.coordinate);
+    setActiveFylke(
+      fylkeUnderPointer.length > 0 ? fylkeUnderPointer[0] : undefined,
+    );
+  }
 
   useEffect(() => {
     map.setTarget(mapRef.current!);
+    map.on("pointermove", handlePointermove);
   }, []);
 
   return (
     <>
-      <h1>Kart over administrative områder i Norge</h1>
+      <h1>
+        {activeFylke
+          ? activeFylke.getProperties()["fylkesnavn"]
+          : "Kart over administrative områder i Norge"}
+      </h1>
       <div ref={mapRef}></div>
     </>
   );
