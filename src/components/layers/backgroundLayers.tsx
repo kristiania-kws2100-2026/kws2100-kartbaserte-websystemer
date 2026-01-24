9cf6f3ae165996d78ecdede5f3ee19f8a8934f8f
Johannes Brodwall <jhannes@gmail.com>
2026-01-24 15:33:35 +0100

    show arial photo map

--- src/components/layers/backgroundLayers.tsx
+++ src/components/layers/backgroundLayers.tsx
@@ -4,8 +4,17 @@ import TileLayer from "ol/layer/Tile.js";
 import { OSM, StadiaMaps, WMTS } from "ol/source.js";
 import { WMTSCapabilities } from "ol/format.js";
 import { optionsFromCapabilities } from "ol/source/WMTS.js";
+import proj4 from "proj4";
+import { register } from "ol/proj/proj4.js";
+
+proj4.defs(
+  "urn:ogc:def:crs:EPSG::25833",
+  "+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs",
+);
+register(proj4);
 
 const osmLayer = new TileLayer({ source: new OSM() });
+
 const kartverket = new TileLayer({});
 const kartverketUrl =
   "https://cache.kartverket.no/v1/wmts/1.0.0/WMTSCapabilities.xml";
@@ -21,9 +30,26 @@ fetch(kartverketUrl).then(async (response) => {
   );
 });
 
+const flyfoto = new TileLayer({});
+const flyfotoUrl =
+  "http://opencache.statkart.no/gatekeeper/gk/gk.open_nib_utm33_wmts_v2?SERVICE=WMTS&REQUEST=GetCapabilities";
+fetch(flyfotoUrl).then(async (response) => {
+  const parser = new WMTSCapabilities();
+  const capabilities = parser.read(await response.text());
+  flyfoto.setSource(
+    new WMTS(
+      optionsFromCapabilities(capabilities, {
+        layer: "Nibcache_UTM33_EUREF89_v2",
+        matrixSet: "default028mm",
+      })!,
+    ),
+  );
+});
+
 const LayerOptions = {
   osmLayer: "OpenStreetMap bakgrunnskart",
   kartverket: "Kartverkets bakgrunnskart",
+  flyfoto: "Flyfoto",
   stadiaLayer: "Stadia backgrunnskart",
 };
 type LayerName = keyof typeof LayerOptions;
@@ -54,13 +80,14 @@ export function BackgroundLayerSelect({
     () => ({
       osmLayer,
       kartverket,
+      flyfoto,
       stadiaLayer,
     }),
     [stadiaLayer],
   );
 
   const [layerName, setLayerName] =
-    useState<keyof typeof LayerOptions>("osmLayer");
+    useState<keyof typeof LayerOptions>("flyfoto");
   useEffect(
     () => setBackgroundLayers([layers[layerName]!]),
     [layerName, layers],
