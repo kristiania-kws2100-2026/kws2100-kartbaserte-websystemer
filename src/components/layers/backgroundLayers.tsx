66b4d19b36d26b2b177603b3dfb65c1af2b5dba7
Johannes Brodwall <jhannes@gmail.com>
2026-01-24 15:12:16 +0100

    use computer theme for dark/light background map

--- src/components/layers/backgroundLayers.tsx
+++ src/components/layers/backgroundLayers.tsx
@@ -4,17 +4,10 @@ import TileLayer from "ol/layer/Tile.js";
 import { OSM, StadiaMaps } from "ol/source.js";
 
 const osmLayer = new TileLayer({ source: new OSM() });
-const stadiaLayer = new TileLayer({
-  source: new StadiaMaps({ layer: "alidade_smooth" }),
-});
-const stadiaDarkLayer = new TileLayer({
-  source: new StadiaMaps({ layer: "alidade_smooth_dark" }),
-});
 
 const LayerOptions = {
   osmLayer: "OpenStreetMap bakgrunnskart",
   stadiaLayer: "Stadia backgrunnskart",
-  stadiaDarkLayer: "Stadia backgrunnskart (mørk)",
 };
 type LayerName = keyof typeof LayerOptions;
 
@@ -23,18 +16,37 @@ export function BackgroundLayerSelect({
 }: {
   setBackgroundLayers: (value: Layer[]) => void;
 }) {
-  const layers = useMemo<Record<string, Layer>>(
+  const [darkMode, setDarkMode] = useState(false);
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    setDarkMode(mediaQuery.matches);
+    mediaQuery.addEventListener("change", (event) =>
+      setDarkMode(event.matches),
+    );
+  }, []);
+
+  const stadiaLayer = useMemo(() => {
+    return new TileLayer({
+      source: new StadiaMaps({
+        layer: darkMode ? "alidade_smooth_dark" : "alidade_smooth",
+      }),
+    });
+  }, [darkMode]);
+
+  const layers = useMemo<Record<LayerName, Layer>>(
     () => ({
       osmLayer,
       stadiaLayer,
-      stadiaDarkLayer,
     }),
-    [],
+    [stadiaLayer],
   );
 
   const [layerName, setLayerName] =
     useState<keyof typeof LayerOptions>("osmLayer");
-  useEffect(() => setBackgroundLayers([layers[layerName]!]), [layerName]);
+  useEffect(
+    () => setBackgroundLayers([layers[layerName]!]),
+    [layerName, layers],
+  );
 
   return (
     <select onChange={(e) => setLayerName(e.target.value as LayerName)}>
@@ -43,7 +55,6 @@ export function BackgroundLayerSelect({
           {label}
         </option>
       ))}
-      s{" "}
     </select>
   );
 }
