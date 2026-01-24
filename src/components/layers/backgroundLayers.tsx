d6de989817449795fa11e34c14211603388e6daf
Johannes Brodwall <jhannes@gmail.com>
2026-01-24 14:49:24 +0100

    show stadia background maps

--- src/components/layers/backgroundLayers.tsx
+++ src/components/layers/backgroundLayers.tsx
@@ -1,21 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import type { Layer } from "ol/layer.js";
 import TileLayer from "ol/layer/Tile.js";
-import { OSM } from "ol/source.js";
+import { OSM, StadiaMaps } from "ol/source.js";
+
+const osmLayer = new TileLayer({ source: new OSM() });
+const stadiaLayer = new TileLayer({
+  source: new StadiaMaps({ layer: "alidade_smooth" }),
+});
+const stadiaDarkLayer = new TileLayer({
+  source: new StadiaMaps({ layer: "alidade_smooth_dark" }),
+});
+
+const LayerOptions = {
+  osmLayer: "OpenStreetMap bakgrunnskart",
+  stadiaLayer: "Stadia backgrunnskart",
+  stadiaDarkLayer: "Stadia backgrunnskart (mørk)",
+};
+type LayerName = keyof typeof LayerOptions;
 
 export function BackgroundLayerSelect({
   setBackgroundLayers,
 }: {
   setBackgroundLayers: (value: Layer[]) => void;
 }) {
-  useEffect(() => {
-    setBackgroundLayers([new TileLayer({ source: new OSM() })]);
-  }, []);
+  const layers = useMemo<Record<string, Layer>>(
+    () => ({
+      osmLayer,
+      stadiaLayer,
+      stadiaDarkLayer,
+    }),
+    [],
+  );
+
+  const [layerName, setLayerName] =
+    useState<keyof typeof LayerOptions>("osmLayer");
+  useEffect(() => setBackgroundLayers([layers[layerName]!]), [layerName]);
 
   return (
-    <select>
-      <option>OpenStreetMap bakgrunnskart</option>
-      <option>Stadia bakgrunnskart</option>
+    <select onChange={(e) => setLayerName(e.target.value as LayerName)}>
+      {Object.entries(LayerOptions).map(([key, label]) => (
+        <option key={key} value={key}>
+          {label}
+        </option>
+      ))}
+      s{" "}
     </select>
   );
 }
