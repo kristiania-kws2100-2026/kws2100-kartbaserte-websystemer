b85a72d82f44941a7054fb1dcf8e0760977754cb
Johannes Brodwall <jhannes@gmail.com>
2026-01-26 13:51:25 +0100

    aerial photo map from kartverket

--- src/components/layers/backgroundLayerSelect.tsx
+++ src/components/layers/backgroundLayerSelect.tsx
@@ -4,6 +4,14 @@ import TileLayer from "ol/layer/Tile.js";
 import { OSM, StadiaMaps, WMTS } from "ol/source.js";
 import { WMTSCapabilities } from "ol/format.js";
 import { optionsFromCapabilities } from "ol/source/WMTS.js";
+import proj4 from "proj4";
+import { register } from "ol/proj/proj4.js";
+
+proj4.defs(
+  "EPSG:25832",
+  "+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs",
+);
+register(proj4);
 
 const osmLayer = new TileLayer({ source: new OSM() });
 
@@ -28,6 +36,21 @@ fetch("https://cache.kartverket.no/v1/wmts/1.0.0/WMTSCapabilities.xml").then(
     );
   },
 );
+const aerialLayer = new TileLayer();
+fetch(
+  "http://opencache.statkart.no/gatekeeper/gk/gk.open_nib_utm32_wmts_v2?SERVICE=WMTS&REQUEST=GetCapabilities",
+).then(async (res) => {
+  const parser = new WMTSCapabilities();
+  const capabilities = parser.read(await res.text());
+  aerialLayer.setSource(
+    new WMTS(
+      optionsFromCapabilities(capabilities, {
+        layer: "Nibcache_UTM32_EUREF89_v2",
+        matrixSet: "default028mm",
+      })!,
+    ),
+  );
+});
 
 export function BackgroundLayerSelect({
   setBackgroundLayer,
@@ -44,6 +67,8 @@ export function BackgroundLayerSelect({
       setBackgroundLayer(stadiaLayer);
     } else if (backgroundLayerValue === "osm") {
       setBackgroundLayer(osmLayer);
+    } else if (backgroundLayerValue === "aerial") {
+      setBackgroundLayer(aerialLayer);
     } else if (backgroundLayerValue === "kartverket") {
       setBackgroundLayer(kartverketLayer);
     }
@@ -57,6 +82,7 @@ export function BackgroundLayerSelect({
       <option value={"osm"}>OpenStreetMap bakgrunn</option>
       <option value={"stadia"}>Stadia bakgrunnskart</option>
       <option value={"kartverket"}>Kartverket bakgrunnskart</option>
+      <option value={"aerial"}>Kartverket flyfoto</option>
     </select>
   );
 }
