d997c49e7d97109e32566f7708df645b5b7c75d4
Johannes Brodwall <jhannes@gmail.com>
2026-02-02 12:53:26 +0100

    get ready for dynamic layers

--- src/components/app/application.tsx
+++ src/components/app/application.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile.js";
 import { OSM } from "ol/source.js";
@@ -6,17 +6,27 @@ import { useGeographic } from "ol/proj.js";
 
 // @ts-ignore
 import "ol/ol.css";
+import { Layer } from "ol/layer.js";
+import VectorLayer from "ol/layer/Vector.js";
 
 useGeographic();
 
 const view = new View({ center: [10.9, 59.9], zoom: 12 });
-const map = new Map({
-  view: view,
-  layers: [new TileLayer({ source: new OSM() })],
-});
+const map = new Map({ view });
+
+const osmLayer = new TileLayer({ source: new OSM() });
+
+const kartverketLayer = new TileLayer();
+
+const bydelLayer = new VectorLayer();
+const skoleLayer = new VectorLayer();
 
 export function Application() {
   const mapRef = useRef<HTMLDivElement | null>(null);
+
+  const [layers, setLayers] = useState<Layer[]>([osmLayer]);
+  useEffect(() => map.setLayers(layers), [layers]);
+
   useEffect(() => {
     map.setTarget(mapRef.current!);
     navigator.geolocation.getCurrentPosition((pos) => {
