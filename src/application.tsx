a74501efc8624016e60942a9c4f79246677922bb
Johannes Brodwall <jhannes@gmail.com>
2026-02-08 17:24:20 +0100

    minimal drawing functionality

--- src/application.tsx
+++ src/application.tsx
@@ -5,12 +5,19 @@ import { OSM } from "ol/source.js";
 import { useGeographic } from "ol/proj.js";
 
 import "ol/ol.css";
+import VectorLayer from "ol/layer/Vector.js";
+import VectorSource from "ol/source/Vector.js";
+import { Draw } from "ol/interaction.js";
 
 useGeographic();
 
+const drawingVectorSource = new VectorSource();
+const drawingLayer = new VectorLayer({
+  source: drawingVectorSource,
+});
 const map = new Map({
   view: new View({ center: [10.7, 59.9], zoom: 12 }),
-  layers: [new TileLayer({ source: new OSM() })],
+  layers: [new TileLayer({ source: new OSM() }), drawingLayer],
 });
 
 export function Application() {
@@ -19,5 +26,18 @@ export function Application() {
     map.setTarget(mapRef.current!);
   }, []);
 
-  return <div ref={mapRef}></div>;
+  function handleClick() {
+    map.addInteraction(
+      new Draw({ type: "Point", source: drawingVectorSource }),
+    );
+  }
+
+  return (
+    <>
+      <nav>
+        <button onClick={handleClick}>Draw point</button>
+      </nav>
+      <div ref={mapRef}></div>
+    </>
+  );
 }
