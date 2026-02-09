cd6b6b5e741da9c65723551dbe6ab04d018add61
Johannes Brodwall <jhannes@gmail.com>
2026-02-09 13:00:30 +0100

    save features to localStorage

--- src/application.tsx
+++ src/application.tsx
@@ -8,6 +8,9 @@ import "ol/ol.css";
 import VectorLayer from "ol/layer/Vector.js";
 import VectorSource from "ol/source/Vector.js";
 import { Draw } from "ol/interaction.js";
+import { GeoJSON } from "ol/format.js";
+
+const geoJson = new GeoJSON();
 
 useGeographic();
 
@@ -15,6 +18,10 @@ const drawingVectorSource = new VectorSource();
 const drawingLayer = new VectorLayer({
   source: drawingVectorSource,
 });
+const savedFeatures = localStorage.getItem("features");
+if (savedFeatures) {
+  drawingVectorSource.addFeatures(geoJson.readFeatures(savedFeatures));
+}
 
 const map = new Map({
   view: new View({ center: [10.7, 59.9], zoom: 12 }),
@@ -25,6 +32,12 @@ export function Application() {
   const mapRef = useRef<HTMLDivElement | null>(null);
   useEffect(() => {
     map.setTarget(mapRef.current!);
+    drawingVectorSource.on("change", () => {
+      localStorage.setItem(
+        "features",
+        geoJson.writeFeatures(drawingVectorSource.getFeatures()),
+      );
+    });
   }, []);
 
   function handleClick() {
