44f04c4586a18e3814b374d478fd59d587a44ab4
Johannes Brodwall <jhannes@gmail.com>
2026-02-09 12:51:54 +0100

    add an interaction to draw on the map

--- src/application.tsx
+++ src/application.tsx
@@ -5,12 +5,20 @@ import { useEffect, useRef } from "react";
 import { useGeographic } from "ol/proj.js";
 
 import "ol/ol.css";
+import VectorLayer from "ol/layer/Vector.js";
+import VectorSource from "ol/source/Vector.js";
+import { Draw } from "ol/interaction.js";
 
 useGeographic();
 
+const drawingVectorSource = new VectorSource();
+const drawingLayer = new VectorLayer({
+  source: drawingVectorSource,
+});
+
 const map = new Map({
   view: new View({ center: [10.7, 59.9], zoom: 12 }),
-  layers: [new TileLayer({ source: new OSM() })],
+  layers: [new TileLayer({ source: new OSM() }), drawingLayer],
 });
 
 export function Application() {
@@ -18,5 +26,20 @@ export function Application() {
   useEffect(() => {
     map.setTarget(mapRef.current!);
   }, []);
-  return <div ref={mapRef}></div>;
+
+  function handleClick() {
+    map.addInteraction(
+      new Draw({
+        type: "Point",
+        source: drawingVectorSource,
+      }),
+    );
+  }
+
+  return (
+    <>
+      <button onClick={handleClick}>Add point</button>
+      <div ref={mapRef}></div>
+    </>
+  );
 }
