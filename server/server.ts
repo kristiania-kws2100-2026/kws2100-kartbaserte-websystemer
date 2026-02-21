1f9db655108e6cee14611dcc5fc1fa935934b64e
Johannes Brodwall <jhannes@gmail.com>
2026-02-21 13:23:09 +0100

    only show schools in the historical Viken fylke

--- server/server.ts
+++ server/server.ts
@@ -21,7 +21,12 @@ function toFeatureCollection(rows: (object & { geometry: any })[]) {
 
 app.get("/api/grunnskoler", async (c) => {
   const result = await postgresql.query(
-    "select skolenavn, eierforhold, antallelever, posisjon::json as geometry from grunnskoler_1a0d38b7214c4b4fbd0bc9f3a77f3ad6.grunnskole",
+    `select a.navn, skolenavn, eierforhold, antallelever, posisjon::json as geometry
+     from grunnskoler_1a0d38b7214c4b4fbd0bc9f3a77f3ad6.grunnskole s
+            inner join fylker_ba7aea2735714391a98b1a585644e98a.fylke f on st_contains(f.omrade, s.posisjon)
+            inner join fylker_ba7aea2735714391a98b1a585644e98a.administrativenhetnavn a on f.objid = a.fylke_fk
+       and a.sprak = 'nor'
+     where a.navn = 'Viken'`,
   );
   return c.json(toFeatureCollection(result.rows));
 });
