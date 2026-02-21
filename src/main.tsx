10c6c3f84fa81cf924e617f124690bb3f9214d77
Johannes Brodwall <jhannes@gmail.com>
2026-02-21 13:02:36 +0100

    display /api/grunnskoler in frontend

--- src/main.tsx
+++ src/main.tsx
@@ -9,19 +9,31 @@ import "ol/ol.css";
 import VectorLayer from "ol/layer/Vector.js";
 import VectorSource from "ol/source/Vector.js";
 import { GeoJSON } from "ol/format.js";
+import { register } from "ol/proj/proj4.js";
+import proj4 from "proj4";
+
+proj4.defs(
+  "EPSG:25833",
+  "+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs",
+);
+register(proj4);
 
 useGeographic();
+const kommuneLayer = new VectorLayer({
+  source: new VectorSource({
+    url: "/geojson/kommuner.geojson",
+    format: new GeoJSON(),
+  }),
+});
+const grunnskoleLayer = new VectorLayer({
+  source: new VectorSource({
+    url: "/api/grunnskoler",
+    format: new GeoJSON({ dataProjection: "EPSG:25833" }),
+  }),
+});
 const map = new Map({
   view: new View({ center: [10.7, 59.9], zoom: 10 }),
-  layers: [
-    new TileLayer({ source: new OSM() }),
-    new VectorLayer({
-      source: new VectorSource({
-        url: "/geojson/kommuner.geojson",
-        format: new GeoJSON(),
-      }),
-    }),
-  ],
+  layers: [new TileLayer({ source: new OSM() }), kommuneLayer, grunnskoleLayer],
 });
 
 function Application() {
