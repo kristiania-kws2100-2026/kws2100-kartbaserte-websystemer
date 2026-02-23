1c32faa487ce9eae63c2052c0b9ff4a09efd16af
Johannes Brodwall <jhannes@gmail.com>
2026-02-23 13:51:11 +0100

    filter grunnskoler to the old fylke Viken

--- server/server.ts
+++ server/server.ts
@@ -9,8 +9,13 @@ const app = new Hono();
 app.get("/api/grunnskoler", async (c) => {
   const result = await postgres.query(
     `select  skolenavn, eierforhold, antallelever, st_transform(posisjon, 4326)::json as geometry
-     from grunnskoler_1a0d38b7214c4b4fbd0bc9f3a77f3ad6.grunnskole s
-    `,
+     from
+       grunnskoler_1a0d38b7214c4b4fbd0bc9f3a77f3ad6.grunnskole s
+         inner join fylker_ba7aea2735714391a98b1a585644e98a.fylke f on st_contains(f.omrade, s.posisjon)
+         join fylker_ba7aea2735714391a98b1a585644e98a.administrativenhetnavn a on f.objid = a.fylke_fk
+         and a.sprak = 'nor'
+     where a.navn = 'Viken'
+`,
   );
   return c.json({
     type: "FeatureCollection",
