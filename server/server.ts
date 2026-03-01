a8b7144b71f742dff5ff7b3c1cfb2816a8602c76
Johannes Brodwall <jhannes@gmail.com>
2026-03-01 22:36:34 +0100

    update school schema

--- server/server.ts
+++ server/server.ts
@@ -14,7 +14,7 @@ app.get("/api/grunnskoler", async (c) => {
   const result = await postgres.query(
     `select  skolenavn, eierforhold, antallelever, st_transform(posisjon, 4326)::json as geometry
      from
-       grunnskoler_1a0d38b7214c4b4fbd0bc9f3a77f3ad6.grunnskole s
+       grunnskoler_519889439f4c490fab3f18303772a702.grunnskole s
          inner join fylker_ba7aea2735714391a98b1a585644e98a.fylke f on st_contains(f.omrade, s.posisjon)
          join fylker_ba7aea2735714391a98b1a585644e98a.administrativenhetnavn a on f.objid = a.fylke_fk
          and a.sprak = 'nor'
