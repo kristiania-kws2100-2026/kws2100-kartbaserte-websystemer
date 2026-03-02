2597d2a8ea1465fdd2816c848d4671a99903f120
Johannes Brodwall <jhannes@gmail.com>
2026-03-02 13:24:56 +0100

    show grunnskoler from database

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,32 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 
+import pg from "pg";
+
+const postgres = new pg.Pool({
+  user: "postgres",
+});
+
 const app = new Hono();
 app.get("/", (c) => c.text("Hello world"));
-app.get("/api/grunnskoler", (c) =>
-  c.json({
+app.get("/api/grunnskoler", async (c) => {
+  const result = await postgres.query(`
+    select skolenavn,
+           antallelever,
+           st_transform(posisjon, 4326)::json posisjon
+    from grunnskoler_519889439f4c490fab3f18303772a702.grunnskole
+  `);
+  return c.json({
     type: "FeatureCollection",
-  }),
-);
+    features: result.rows.map(({ skolenavn, antallelever, posisjon }) => ({
+      type: "Feature",
+      properties: {
+        skolenavn,
+        antallelever,
+      },
+      geometry: posisjon,
+    })),
+  });
+});
 
 serve(app);
