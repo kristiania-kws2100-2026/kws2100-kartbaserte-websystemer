70a8f7751aa2003432a8f646a64f90eb73cbe341
Johannes Brodwall <jhannes@gmail.com>
2026-03-02 12:57:23 +0100

    define the server and the connection from the client

---
+++ server/server.ts
@@ -0,0 +1,12 @@
+import { Hono } from "hono";
+import { serve } from "@hono/node-server";
+
+const app = new Hono();
+app.get("/", (c) => c.text("Hello world"));
+app.get("/api/grunnskoler", (c) =>
+  c.json({
+    type: "FeatureCollection",
+  }),
+);
+
+serve(app);
