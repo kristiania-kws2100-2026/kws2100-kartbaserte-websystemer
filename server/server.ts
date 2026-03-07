516206bb4284aae41ea0758ae54f4b7e1e8ddc1f
Johannes Brodwall <jhannes@gmail.com>
2026-03-07 14:01:39 +0100

    refactor /api/grunnskoler to be similar to /api/kommuner

--- server/server.ts
+++ server/server.ts
@@ -15,24 +15,23 @@ app.get("/api/grunnskoler", async (c) => {
   const result = await postgres.query(`
     select skolenavn,
            antallelever,
-           st_transform(posisjon, 4326)::json posisjon
+           st_transform(posisjon, 4326)::json geometry
     from grunnskoler_26f23a96d4914f1dbde464c9bd921e8c.grunnskole
   `);
   return c.json({
     type: "FeatureCollection",
-    features: result.rows.map(({ skolenavn, antallelever, posisjon }) => ({
+    features: result.rows.map(({ geometry, ...properties }) => ({
       type: "Feature",
-      properties: {
-        skolenavn,
-        antallelever,
-      },
-      geometry: posisjon,
+      properties,
+      geometry,
     })),
   });
 });
 app.get("/api/kommuner", async (c) => {
   const result = await postgres.query(`
-    select kommunenummer, kommunenavn, st_transform(omrade, 4326)::json geometry
+    select kommunenummer,
+           kommunenavn,
+           st_transform(omrade, 4326)::json geometry
     from kommuner_627ee106072240e99d2b21ec4717bf01.kommune
   `);
   return c.json({
