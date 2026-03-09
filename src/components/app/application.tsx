ac042053a8aa6f3cc59fa19e080826ad69ed7f36
Johannes Brodwall <jhannes@gmail.com>
2026-03-09 23:15:22 +0100

    show grunnkretser in map

--- src/components/app/application.tsx
+++ src/components/app/application.tsx
@@ -5,11 +5,22 @@ import { useGeographic } from "ol/proj.js";
 import { useEffect, useRef } from "react";
 
 import "ol/ol.css";
+import VectorLayer from "ol/layer/Vector.js";
+import VectorSource from "ol/source/Vector.js";
+import { GeoJSON } from "ol/format.js";
 
 useGeographic();
 
 const map = new Map({
-  layers: [new TileLayer({ source: new OSM() })],
+  layers: [
+    new TileLayer({ source: new OSM() }),
+    new VectorLayer({
+      source: new VectorSource({
+        url: "/api/grunnkrets",
+        format: new GeoJSON(),
+      }),
+    }),
+  ],
   view: new View({ center: [10.7, 59.9], zoom: 11 }),
 });
 
