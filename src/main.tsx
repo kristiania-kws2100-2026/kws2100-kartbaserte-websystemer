0c0bdc66eee9f65314505421f9570043802fecd5
Johannes Brodwall <jhannes@gmail.com>
2026-03-16 13:28:07 +0100

    show heat map based on the number of adresses

--- src/main.tsx
+++ src/main.tsx
@@ -11,9 +11,21 @@ import type { FeatureLike } from "ol/Feature.js";
 import VectorLayer from "ol/layer/Vector.js";
 import VectorSource from "ol/source/Vector.js";
 import { GeoJSON } from "ol/format.js";
+import { Fill, Stroke, Style } from "ol/style.js";
 
 useGeographic();
 const backgroundLayer = new TileLayer({ source: new OSM() });
+
+function getColor(properties: any) {
+  console.log(properties);
+  const antall_adresser = parseInt(properties.antall_adresser);
+  if (antall_adresser < 20) {
+    return "rgba(255, 255, 255, 0.5)";
+  }
+  const percentage = antall_adresser / 900.0;
+  return `rgba(${192 * percentage}, ${192 * (1 - percentage)}, 0, 0.5)`;
+}
+
 const map = new Map({
   view: new View({ center: [10.7, 59.9], zoom: 11 }),
   layers: [
@@ -23,6 +35,13 @@ const map = new Map({
         url: "/api/skolerapport",
         format: new GeoJSON(),
       }),
+      style: (feature) =>
+        new Style({
+          stroke: new Stroke({ width: 2, color: "black" }),
+          fill: new Fill({
+            color: getColor(feature.getProperties()),
+          }),
+        }),
     }),
   ],
 });
