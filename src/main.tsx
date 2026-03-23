370988ea2c6d801bab1d6c25fe3a2f6de37fe98d
Johannes Brodwall <jhannes@gmail.com>
2026-03-23 12:28:38 +0100

    add typescript

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,3 @@
 import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById("app")).render(<h1>Hello Public Transport</h1>)
+createRoot(document.getElementById("app")!).render(<h1>Hello Public Transport</h1>)
