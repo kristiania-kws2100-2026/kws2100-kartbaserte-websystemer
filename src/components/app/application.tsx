fb800c836dca2263218a4a048829fc23d4f7403b
Johannes Brodwall <jhannes@gmail.com>
2026-03-23 09:06:34 +0100

    create OpenLayers application

--- src/components/app/application.tsx
+++ src/components/app/application.tsx
@@ -1,3 +1,18 @@
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile.js";
+import { OSM } from "ol/source.js";
+import { useEffect, useRef } from "react";
+import { useGeographic } from "ol/proj.js";
+
+useGeographic();
+
+const map = new Map({
+  layers: [new TileLayer({ source: new OSM() })],
+  view: new View({ center: [10.7, 59.9], zoom: 10 }),
+});
+
 export function Application() {
-  return <h1>Hello React</h1>;
+  const mapRef = useRef<HTMLDivElement | null>(null);
+  useEffect(() => map.setTarget(mapRef.current!), []);
+  return <div ref={mapRef}></div>;
 }
