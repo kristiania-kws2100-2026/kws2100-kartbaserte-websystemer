d6d94627fbeec8c8fcbbbbc36fc9d811e83a3d20
Johannes Brodwall <jhannes@gmail.com>
2026-03-23 09:48:35 +0100

    click on map to display vehicle properties

--- src/components/app/application.tsx
+++ src/components/app/application.tsx
@@ -1,7 +1,7 @@
-import { Feature, Map, View } from "ol";
+import { Feature, Map, MapBrowserEvent, View } from "ol";
 import TileLayer from "ol/layer/Tile.js";
 import { OSM } from "ol/source.js";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useGeographic } from "ol/proj.js";
 
 import "ol/ol.css";
@@ -10,10 +10,21 @@ import VectorLayer from "ol/layer/Vector.js";
 import { Pointer } from "ol/interaction.js";
 import { Point } from "ol/geom.js";
 import VectorSource from "ol/source/Vector.js";
+import type { FeatureLike } from "ol/Feature.js";
+import { Fill, RegularShape, Stroke, Style } from "ol/style.js";
 
 useGeographic();
 
-const vehicleLayer = new VectorLayer();
+const vehicleLayer = new VectorLayer({
+  style: new Style({
+    image: new RegularShape({
+      points: 4,
+      radius: 7,
+      fill: new Fill({ color: "blue" }),
+      stroke: new Stroke({ color: "white" }),
+    }),
+  }),
+});
 
 const map = new Map({
   layers: [new TileLayer({ source: new OSM() }), vehicleLayer],
@@ -22,13 +33,29 @@ const map = new Map({
 
 export function Application() {
   const mapRef = useRef<HTMLDivElement | null>(null);
+  const [selectedFeatures, setSelectedFeatures] = useState<FeatureLike[]>([]);
   useEffect(() => {
     map.setTarget(mapRef.current!);
     setInterval(() => {
       fetchData();
     }, 15_000);
+    map.on("click", (e: MapBrowserEvent) => {
+      const features = map.getFeaturesAtPixel(e.pixel, {});
+      setSelectedFeatures(features);
+    });
   }, []);
-  return <div ref={mapRef}></div>;
+  return (
+    <div ref={mapRef}>
+      <div>
+        Selected vehicles:{" "}
+        {selectedFeatures
+          .map((f) => f.getProperties())
+          .map(({ geometry, ...properties }) => (
+            <pre>{JSON.stringify(properties)}</pre>
+          ))}
+      </div>
+    </div>
+  );
 }
 
 async function fetchData() {
@@ -38,10 +65,14 @@ async function fetchData() {
   const features = FeedMessage.decode(
     new Uint8Array(await res.arrayBuffer()),
   ).entity.map((e) => {
-    const { latitude, longitude } = e.vehicle!.position!;
-    return new Feature({ geometry: new Point([longitude, latitude]) });
+    const { latitude, longitude, speed } = e.vehicle!.position!;
+    const routeId = e.vehicle?.trip?.routeId!;
+    const properties = { routeId, speed, type: "vehicle" };
+    return new Feature({
+      geometry: new Point([longitude, latitude]),
+      ...properties,
+    });
   });
-  console.log(features);
   vehicleLayer.setSource(new VectorSource({ features }));
 }
 
