f6ae293f016debf29b94b29321e0737edce10ad4
Johannes Brodwall <jhannes@gmail.com>
2026-03-23 12:58:02 +0100

    generate OpenLayer Features from GTFS-RT protobuf message entity

--- src/components/app/application.tsx
+++ src/components/app/application.tsx
@@ -1,4 +1,4 @@
-import { Map, View } from "ol";
+import { Feature, Map, View } from "ol";
 import TileLayer from "ol/layer/Tile.js";
 import { OSM } from "ol/source.js";
 import VectorLayer from "ol/layer/Vector.js";
@@ -6,6 +6,9 @@ import { useEffect, useRef } from "react";
 import { useGeographic } from "ol/proj.js";
 
 import "ol/ol.css";
+import { FeedMessage } from "../../../generated/gtfs-realtime.js";
+import { Point } from "ol/geom.js";
+import VectorSource from "ol/source/Vector.js";
 
 useGeographic();
 
@@ -20,3 +23,16 @@ export function Application() {
   useEffect(() => map.setTarget(mapRef.current!), []);
   return <div ref={mapRef}></div>;
 }
+
+async function loadVehicles() {
+  const res = await fetch(
+    "https://api.entur.io/realtime/v1/gtfs-rt/vehicle-positions",
+  );
+  const message = FeedMessage.decode(Uint8Array.from(await res.bytes()));
+  const features = message.entity.map((e) => {
+    const { latitude, longitude } = e.vehicle?.position!;
+    return new Feature({ geometry: new Point([longitude, latitude]) });
+  });
+  vehicleLayer.setSource(new VectorSource({ features }));
+}
+loadVehicles();
