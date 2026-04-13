36a8c6abbcdc5724142c0aae5551991c17628d2e
Johannes Brodwall <jhannes@gmail.com>
2026-04-13 09:49:52 +0200

    intermezzo: display school properties on click

--- src/application.tsx
+++ src/application.tsx
@@ -1,15 +1,17 @@
-import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile.js";
-import { OSM } from "ol/source.js";
-import { useEffect, useRef } from "react";
-import { useGeographic } from "ol/proj.js";
 import VectorLayer from "ol/layer/Vector.js";
 import VectorSource from "ol/source/Vector.js";
 import VectorTileLayer from "ol/layer/VectorTile.js";
 import VectorTileSource from "ol/source/VectorTile.js";
+import type { FeatureLike } from "ol/Feature.js";
 import { GeoJSON } from "ol/format.js";
+import { Map, MapBrowserEvent, Overlay, View } from "ol";
+import { OSM } from "ol/source.js";
+import { useEffect, useRef, useState } from "react";
+import { useGeographic } from "ol/proj.js";
 
 import "ol/ol.css";
+import "./application.css";
 
 useGeographic();
 
@@ -31,11 +33,37 @@ const map = new Map({
   ],
   view: new View({ center: [10.7, 59.9], zoom: 12 }),
 });
+const overlay = new Overlay({
+  positioning: "top-center",
+});
 
 export function Application() {
   const mapRef = useRef<HTMLDivElement | null>(null);
+  const overlayRef = useRef<HTMLDivElement | null>(null);
+  const [selectedGrunnskoler, setSelectedGrunnskoler] = useState<FeatureLike[]>(
+    [],
+  );
 
-  useEffect(() => map.setTarget(mapRef.current!));
+  useEffect(() => {
+    map.setTarget(mapRef.current!);
+    overlay.setElement(overlayRef.current!);
+    map.addOverlay(overlay);
 
-  return <div ref={mapRef}></div>;
+    map.on("click", (e: MapBrowserEvent) => {
+      const features = map.getFeaturesAtPixel(e.pixel);
+      setSelectedGrunnskoler(features);
+      overlay.setPosition(features.length > 0 ? e.coordinate : undefined);
+    });
+  });
+
+  return (
+    <div ref={mapRef}>
+      <div ref={overlayRef}>
+        Selected school:{" "}
+        {selectedGrunnskoler.map((s) => (
+          <div>{s.getProperties()["skolenavn"]}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
