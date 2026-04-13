429eed1af99677af39a895f39d5933dd91bf794e
Johannes Brodwall <jhannes@gmail.com>
2026-04-13 12:57:58 +0200

    implement /api/grunnskole from PostGIS

--- server/server.ts
+++ server/server.ts
@@ -8,8 +8,18 @@ const connectionString = "psql://postgres@localhost";
 const db = new pg.Pool({ connectionString });
 
 app.get("/api/grunnskole", async (c) => {
-  const result = await db.query("select * from spatial_ref_sys");
-  return c.json(result.rows);
+  const result = await db.query(
+    `
+      select skolenavn, organisasjonsnummer, antallelever, st_transform(posisjon, 4326)::json geometry
+      from grunnskoler_ab90da242c084b34aaa0acfbbd6fada6.grunnskole
+    `,
+  );
+  const features = result.rows.map(({ geometry, ...properties }) => ({
+    type: "Feature",
+    geometry,
+    properties,
+  }));
+  return c.json({ type: "FeatureCollection", features });
 });
 
 serve(app);
