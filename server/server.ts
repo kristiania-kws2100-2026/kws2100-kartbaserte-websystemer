1498d14535ac8ee4b4dd5b7a87b93fdd7fbb8194
Johannes Brodwall <jhannes@gmail.com>
2026-04-13 09:31:17 +0200

    connect server to database

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,13 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
+import pg from "pg";
+
+const db = new pg.Pool({ connectionString: "psql://postgres@localhost" });
 
 const app = new Hono();
-app.get("/api/kommuner", (c) => c.json({ hello: "world" }));
+app.get("/api/kommuner", async (c) => {
+  const result = await db.query("select * from spatial_ref_sys");
+  return c.json(result.rows);
+});
 
 serve(app);
