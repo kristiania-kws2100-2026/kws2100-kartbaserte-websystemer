6e4fb7fb4c10797f8aa7a1b0b7d054ec086f815b
Johannes Brodwall <jhannes@gmail.com>
2026-04-13 12:30:49 +0200

    define target for the lecture

--- src/application.tsx
+++ src/application.tsx
@@ -5,11 +5,30 @@ import { useEffect, useRef } from "react";
 import { useGeographic } from "ol/proj.js";
 
 import "ol/ol.css";
+import VectorLayer from "ol/layer/Vector.js";
+import VectorSource from "ol/source/Vector.js";
+import VectorTileSource from "ol/source/VectorTile.js";
+import { GeoJSON, MVT } from "ol/format.js";
+import VectorTileLayer from "ol/layer/VectorTile.js";
 
 useGeographic();
 
 const map = new Map({
-  layers: [new TileLayer({ source: new OSM() })],
+  layers: [
+    new TileLayer({ source: new OSM() }),
+    new VectorLayer({
+      source: new VectorSource({
+        url: "/api/grunnskole",
+        format: new GeoJSON(),
+      }),
+    }),
+    new VectorTileLayer({
+      source: new VectorTileSource({
+        url: "/api/adresse/{z}/{x}/{y}",
+        format: new MVT(),
+      }),
+    }),
+  ],
   view: new View({ center: [10.7, 59.9], zoom: 8 }),
 });
 
