5191896eb2b82b544204708c350c578a0b08bd01
Johannes Brodwall <jhannes@gmail.com>
2026-04-13 09:21:03 +0200

    declare layers to be developed

--- src/application.tsx
+++ src/application.tsx
@@ -3,13 +3,29 @@ import TileLayer from "ol/layer/Tile.js";
 import { OSM } from "ol/source.js";
 import { useEffect, useRef } from "react";
 import { useGeographic } from "ol/proj.js";
+import VectorLayer from "ol/layer/Vector.js";
+import VectorSource from "ol/source/Vector.js";
+import VectorTileLayer from "ol/layer/VectorTile.js";
+import VectorTileSource from "ol/source/VectorTile.js";
+import { GeoJSON } from "ol/format.js";
 
 import "ol/ol.css";
 
 useGeographic();
 
 const map = new Map({
-  layers: [new TileLayer({ source: new OSM() })],
+  layers: [
+    new TileLayer({ source: new OSM() }),
+    new VectorLayer({
+      source: new VectorSource({ url: "/api/kommuner", format: new GeoJSON() }),
+    }),
+    new VectorTileLayer({
+      source: new VectorTileSource({
+        url: "/api/adresser",
+        format: new GeoJSON(),
+      }),
+    }),
+  ],
   view: new View({ center: [10.7, 59.9], zoom: 12 }),
 });
 
