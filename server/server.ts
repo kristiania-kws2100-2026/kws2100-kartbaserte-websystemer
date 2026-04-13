cb37a70ad4c2e02ae5f3295444e18647767aa863
Johannes Brodwall <jhannes@gmail.com>
2026-04-13 09:43:26 +0200

    /api/grunnskoler returns skoler as features

--- server/server.ts
+++ server/server.ts
@@ -7,10 +7,17 @@ const db = new pg.Pool({ connectionString: "psql://postgres@localhost" });
 const app = new Hono();
 app.get("/api/grunnskole", async (c) => {
   const result = await db.query(`
-    select *
+    select skolenavn, organisasjonsnummer, antallelever, st_transform(posisjon, 4326)::json geometry
     from grunnskoler_ab90da242c084b34aaa0acfbbd6fada6.grunnskole
   `);
-  return c.json(result.rows);
+  return c.json({
+    type: "FeatureCollection",
+    features: result.rows.map(({ geometry, ...properties }) => ({
+      type: "Feature",
+      properties,
+      geometry,
+    })),
+  });
 });
 
 serve(app);
