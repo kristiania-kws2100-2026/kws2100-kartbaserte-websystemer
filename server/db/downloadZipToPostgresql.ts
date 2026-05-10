856142462ce5a90d924853c458c9a8713d9f2de2
Johannes Brodwall <jhannes@gmail.com>
2026-05-10 16:28:51 +0200

    debugging: use process.env.TMP as working directory (default to /tmp)

--- server/db/downloadZipToPostgresql.ts
+++ server/db/downloadZipToPostgresql.ts
@@ -26,7 +26,8 @@ export async function downloadZipToPostgresql(
   );
   if (rows.length > 1) throw Error("whops!");
 
-  const file = `${process.env.TMP}/${prefix}.zip`;
+  const tmpDir = process.env.TMP || "/tmp";
+  const file = `${tmpDir}/${prefix}.zip`;
   console.log(`Downloading ${file}`);
   const res = await fetch(url, { headers: { "If-None-Match": rows[0]?.etag } });
   if (res.status === 304) {
@@ -54,17 +55,17 @@ export async function downloadZipToPostgresql(
   const result: Promise<unknown>[] = [];
   zipFile.forEach((entry) => {
     console.log(file + ": " + entry.entryName);
-    zipFile.extractEntryTo(entry, `${process.env.TMP}/.`);
+    zipFile.extractEntryTo(entry, `${tmpDir} || "/tmp/.`);
     const command =
       os.platform() === "win32"
-        ? `cmd /c "docker exec -i kws2100 /usr/bin/psql --user postgres < ${process.env.TMP}/${entry.entryName}"`
-        : `/usr/bin/psql ${connectionString} < ${process.env.TMP}/${entry.entryName}`;
+        ? `cmd /c "docker exec -i kws2100 /usr/bin/psql --user postgres < ${tmpDir} || "/tmp/${entry.entryName}"`
+        : `/usr/bin/psql ${connectionString} < ${tmpDir}/${entry.entryName}`;
     console.log("executing " + command);
     const proc = exec(command);
     proc.stdout?.on("data", (data) => console.log(data));
     const promise = new Promise<void>((resolve, reject) => {
       proc.on("exit", (exitCode) => {
-        fs.unlinkSync(`${process.env.TMP}/${entry.entryName}`);
+        fs.unlinkSync(`${tmpDir}/${entry.entryName}`);
         if (exitCode === 0) return resolve();
         reject(new Error("psql failed with " + exitCode));
       });
